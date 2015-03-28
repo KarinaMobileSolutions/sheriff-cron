126206a2b331e6484018617d4a9654525e2a2662
Sasan Rose <sasan.rose@gmail.com>
2015-03-28 14:16:42 +0430

    Init

---
+++ sheriff.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os/exec"
+)
+
+type Script struct {
+	name      string
+	directory string
+	format    string
+}
+
+var (
+	scripts []Script
+)
+
+func main() {
+	//parseConfig(&scripts)
+
+}
